b7097bba396486a469f1d252920153c7a3396712
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-11-12 11:11:54 +0100

    Proffiling Unity application with Profiler

---
+++ RecrutationInterview/Assets/Scenes/Test1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Profiling;
+
+public class Test1 : MonoBehaviour 
+{
+    void Update()
+    {
+        Profiler.BeginSample("aaaaaaaa");
+        Debug.Log("this code is being profiled");
+        Profiler.EndSample();
+    }
+}
